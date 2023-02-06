549a36c514e88f6ce7b07c1c8cba2c6a91b4d5d7
Enio Schaefer <enio100schaefer@hotmail.com>
2023-02-06 18:04:23 -0300

    [Feat] :rocket: Requisito 04 Concluido

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const validateLogin = require('../middlewares/validateLogin');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', validateLogin, (req, res) => {
   const token = (Math.random().toString(36).substring(2)
     + Math.random().toString(36).substring(2)).substring(0, 16);
 
